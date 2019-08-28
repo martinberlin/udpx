a1daf19597b032ce1bc4ed6b735ff555e8cebe61
martinberlin <martin@luckycloud.de>
2019-08-28 02:10:59 +0200

    Simulate simple animation and export to JSON

--- examples/greensock/script.js
+++ examples/greensock/script.js
@@ -2,10 +2,12 @@ var stripe = $("div#stripe"),
     btn1 = $("button#btn1"),
     btn2 = $("button#btn2"),
     red = $("#r"), green = $("#g"), blue = $("#b"),
+    stripe_length = $("#stripe_length"),
+    json_out = $("#json_out"),
     delay = $("#duration");
 
     tl1 = TweenMax.to(stripe, delay.val(), {
-      left:300,
+      left:288,
       onUpdate:lineUpdate,
       onUpdateParams:["{self}"],
       paused:true
@@ -15,6 +17,23 @@ var stripe = $("div#stripe"),
 function lineUpdate(tween)
 {
   tl1.progress(tween.progress());
+  stripepos = stripe.position();
+  stripex = stripepos.left -10;
+  ledstripex = parseInt((stripex*stripe_length.val())/288);
+  //console.log(ledstripex);
+
+  var json = [];
+  rgb = [parseInt(red.val()), parseInt(green.val()), parseInt(blue.val())];
+  for (var k = 1; k <= parseInt(stripe_length.val()); k++) {
+    if (ledstripex >= k-1 && ledstripex <= k+1) {
+      json.push(rgb);
+    } else {
+      json.push([0,0,0]);
+    }
+    
+  }
+  json_out.val(JSON.stringify(json));
+  
 }
 
 btn1.click(function()
