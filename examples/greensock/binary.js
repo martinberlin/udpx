28902790fea7c99f2c38ddb9272b1305af88b14a
martinberlin <martin@luckycloud.de>
2019-09-02 21:17:09 +0200

    Add per frame controls to measure a single frame speed

--- examples/greensock/binary.js
+++ examples/greensock/binary.js
@@ -2,13 +2,18 @@ var stripe = $("div#stripe"),
     btn1 = $("button#btn1"),
     btn2 = $("button#btn2"),
     btn3 = $("button#btn3"),
+    btnFirst = $("button#btnFirst"),
+    btnMinus = $("button#btnMinus"),
+    btnPlus = $("button#btnPlus"),
+    btnLast = $("button#btnLast"),
     http_mode = $("#http"),
     red = $("#r"), green = $("#g"), blue = $("#b"),
     stripe_length = $("#stripe_length"),
     output = $("#out"),
     ip = $("#esp32_ip"),
     compression = $("#compression"),
-    delay = $("#duration");
+    delay = $("#duration"),
+    iX = 0;
 
     tl1 = TweenMax.to(stripe, delay.val(), {
       left:288,
@@ -28,8 +33,6 @@ function toHexString(byteArray) {
 
 function convertPixel(x) {
   let displayHex = "";
-
-  var bytes = [];
   let off = [0,0,0];
   let pixLength = stripe_length.val();
   rgb = [parseInt(red.val()), parseInt(green.val()), parseInt(blue.val())];
@@ -37,18 +40,12 @@ function convertPixel(x) {
 
   // create an ArrayBuffer with a size in bytes
   var buffer = new ArrayBuffer(bufferLen);
-  // Treat buffer as a view of 8-bit unsigned integer 
   var bytesToPost = new Uint8Array(buffer); 
   bi = 0;
 
-/*
-  Papa explanation of LSB & MSB
-   MSB = Int(Num Dec/256)
-   LSB = Num Dec -(MSB*256)
- */
   let MSB = parseInt(pixLength/256);
   let LSB = pixLength - (MSB*256);
-  // header bytes
+  // header bytes - Todo: Add this in his own function
   hByte = [80,0,0,LSB,MSB];
   console.log(hByte);
   bytesToPost[bi] = hByte[0];bi++;  // p
@@ -64,19 +61,66 @@ function convertPixel(x) {
       bytesToPost[bi] = parseInt(green.val());bi++;
       bytesToPost[bi] = parseInt(blue.val());bi++;
       displayHex += toHexString(rgb);
-      bytes.push(rgb);
       
     } else {
       bytesToPost[bi] = 0;bi++;
       bytesToPost[bi] = 0;bi++;
       bytesToPost[bi] = 0;bi++;
       displayHex += toHexString(off);
-      bytes.push(off);
     }
   }
   
   if (displayHex !== lastPush) { 
-    isHttp = http_mode.is(":checked") ?1:0;
+    sendToEsp(bytesToPost);
+  }
+  output.val(displayHex);
+  lastPush = displayHex;
+}
+
+function xPixel(x) {
+  console.log("Receiving X:" +x);
+  let displayHex = "";
+  let off = [0,0,0];
+  let pixLength = stripe_length.val();
+  rgb = [parseInt(red.val()), parseInt(green.val()), parseInt(blue.val())];
+  let bufferLen = (pixLength*3)+5;
+  let MSB = parseInt(pixLength/256);
+  let LSB = pixLength - (MSB*256);
+  // header bytes
+  hByte = [80,0,0,LSB,MSB];
+  
+  // create an ArrayBuffer with a size in bytes
+  var buffer = new ArrayBuffer(bufferLen);
+  // Treat buffer as a view of 8-bit unsigned integer 
+  var bytesToPost = new Uint8Array(buffer); 
+  bi = 0;
+  bytesToPost[bi] = hByte[0];bi++;  // p
+  bytesToPost[bi] = hByte[1];bi++;  // Future features (not used)
+  bytesToPost[bi] = hByte[2];bi++;  // unsigned 8-bit LED channel number
+  bytesToPost[bi] = hByte[3];bi++;  // count(pixels) 16 bit, next too
+  bytesToPost[bi] = hByte[4];bi++;  // Second part of count(pixels) not used here for now
+  displayHex += toHexString(hByte); // Start the preview in HEX with headers
+
+  for (var k = 0; k <= parseInt(pixLength); k++) {
+    if (x === k) {
+      bytesToPost[bi] = parseInt(red.val());bi++;
+      bytesToPost[bi] = parseInt(green.val());bi++;
+      bytesToPost[bi] = parseInt(blue.val());bi++;
+      displayHex += toHexString(rgb);
+      
+    } else {
+      bytesToPost[bi] = 0;bi++;
+      bytesToPost[bi] = 0;bi++;
+      bytesToPost[bi] = 0;bi++;
+      displayHex += toHexString(off);
+    }
+  }
+  output.val(displayHex);
+  sendToEsp(bytesToPost);
+}
+
+function sendToEsp(bytesToPost) {
+  isHttp = http_mode.is(":checked") ?1:0;
     if (isHttp) {
       // Send using TCP/IP directly to ESP32 IP
       $.ajax("http://"+ ip.val() +"/post",
@@ -100,11 +144,7 @@ function convertPixel(x) {
         contentType: false
         });  
     }
-  }
-  output.val(displayHex);
-  lastPush = displayHex;
 }
-
 function lineUpdate(tween)
 {
   tl1.progress(tween.progress());
@@ -135,4 +175,33 @@ btn2.click(function()
 btn3.click(function()
 {
   convertPixel(10);
+});
+
+btnFirst.click(function()
+{
+  colorIt();
+  iX = 0;
+  tl1.pause(0);
+  xPixel(iX); //iX represents Pixel ID that needs to be on
+});
+btnPlus.click(function()
+{
+  colorIt();
+  iX++;
+  xPixel(iX);
+  console.log("btnPlus");
+});
+btnMinus.click(function()
+{
+  colorIt();
+  iX--;
+  xPixel(iX);
+  console.log("btnMinus");
+});
+btnLast.click(function()
+{
+  colorIt();
+  iX = parseInt(stripe_length.val())-1;
+  //tl1.end(); // TODO: Don't know how to send animation to the end
+  xPixel(iX);
 });
