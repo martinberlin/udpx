db5bd244402b8b426be7a0cb97ce29c4f7107724
Samuel Archibald <samuel.archibald00@gmail.com>
2019-09-11 21:54:25 -0400

    s additions

--- lib/s/src/s.cpp
+++ lib/s/src/s.cpp
@@ -5,8 +5,34 @@ s::s(uint8_t nodeId, uint8_t sessID){
     currentSession = sessID;
 }
 
-void s::setCallback(void(*callbackFnc)(uint8_t *pyld, uint8_t len)){
+void s::setCallback(void(*callbackFnc)(uint8_t *pyld, uint16_t len)){
     callback = callbackFnc;
+}
+
+uint8_t s::receive(uint8_t *pyld, uint16_t len){
+    s::message m = parseMessage(pyld, len);
+    if(m.h.version!=VERSION){
+        return RECEIVE_FAIL_BAD_HEADER;
+    }
+    if(m.h.session==0){
+        return RECEIVE_FAIL_ZERO_SESS;
+    }
+    if(m.h.session!=currentSession){
+        return RECEIVE_FAIL_INVALID_SESSION;
+    }
+    if(m.h.nodeID!=nodeID){
+        return RECEIVE_FAIL_INCORRECT_NODE;
+    }
+    bool rt = recentTransaction(m.h.frame);
+    if(m.h.frame<lastDwnFrame&&!rt){
+        return RECEIVE_FAIL_BAD_FRAME_ID;
+    }
+
+    if(!rt&&m.h.fragment==0){
+        s::transaction t = {transactionIndex++, m.transactionLength, m.pyld, m.transactionChecksum, m.transactionChecksum.frame, true, millis()};
+    }
+
+    return RECEIVE_NO_FAIL;
 }
 
 void s::setNodeID(uint8_t ID){
@@ -35,8 +61,16 @@ s::message s::parseMessage(uint8_t *pyld, uint16_t len){
         m.valid = false;
         return m;
     }
-    m.pyld = &pyld[4];
-    m.len = len - 4;
+    m.len = len - 5;
+    if(m.h.fragment==0){ // If this is the first fragment in the transaction, we want to gather the size and checksum
+        m.transactionLength = pyld[5] | pyld[6]<<8;
+        m.transactionChecksum = pyld[7];
+        m.pyld = &pyld[8];
+        m.len = len - 8;
+    }else{
+        m.pyld = &pyld[5];
+        m.len = len - 5;
+    }
     m.valid = true;
     return m;
 }
@@ -62,6 +96,22 @@ s::header s::parseHeader(uint8_t *pyld){
     h.nodeID = pyld[1];
     h.session = pyld[2];
     h.frame = pyld[3];
+    h.fragment = pyld[4];
     h.valid = true;
     return h;
+}
+
+bool s::recentTransaction(uint8_t frame){
+    for(unsigned i =0; i<transactionBufferSize;i++){
+        if (transactionBuffer[i].frame==frame&&transactionBuffer[i].valid){
+            // We have to loop through all the messages in this function every time we get a message, may as well calculate TTL and expire any entries
+            if(millis()-transactionBuffer[i].started>=TTL){
+                transactionBuffer[i].valid = false;
+                transactionBuffer[i].pyld = NULL;
+                continue;
+            }
+            return true;
+        }
+    }
+    return false;
 }
