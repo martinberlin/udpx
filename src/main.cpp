2f2716eb121b7cd4b7d393b8a7af70a8b8ca9df7
martinberlin <martin@luckycloud.de>
2019-12-07 19:02:20 +0100

    Recover Brotli decompression without S in master. S is living on develop now

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,29 @@
 #include <WiFi.h>
-#include "AsyncUDP.h"
+#include <AsyncUDP.h>
 #include <Config.h>
-#include <FS.h>
-#include "SPIFFS.h"
-#include <ArduinoJson.h>
 #include <pixels.h>
-#include <s.h>
+#include <brotli/decode.h>
 extern "C" {
 	#include "freertos/FreeRTOS.h"
 	#include "freertos/timers.h"
 }
-#include <AsyncMqttClient.h>
+#include <SimpleTimer.h>
 
-AsyncMqttClient mqttClient;
-TimerHandle_t mqttReconnectTimer;
+#define BROTLI_DECOMPRESSION_BUFFER 6000
+TaskHandle_t brotliTask;
+size_t receivedLength;
 TimerHandle_t wifiReconnectTimer;
-String lastWill;
-// Note DynamicJsonDocument should be created once otherwise eats your HEAP memory like cookies
-const size_t capacity = 144 * JSON_ARRAY_SIZE(4) + JSON_ARRAY_SIZE(144);
-DynamicJsonDocument doc(capacity);
-char bssid[9];
-s stream();
-
+unsigned long frameCounter = 0;
+unsigned long frameLastCounter = frameCounter;
+long decompressionFailed = 0;
 // Debug mode prints to serial
 bool debugMode = DEBUG_MODE;
-// Turn on debug to enjoy seeing how ArduinoJson eats your heap memory
 
 // Message transport protocol
 AsyncUDP udp;
-
-// Brotli decompression buffer
-uint8_t * compressed;
-TaskHandle_t brotliTask;
-size_t receivedLength;
-// Output class and Mqtt message buffer
+SimpleTimer timer;
+// Output class pixels
 PIXELS pix;
-String payloadBuffer;
-// SPIFFS to read presentation
-File fsFile;
-String presentation;
-
-struct config {
-  char title[140];
-  bool compression = true;
-} internalConfig;
-
 
 typedef struct {
   unsigned size;
@@ -65,15 +44,54 @@ void printMessage(String message, bool newline = true)
    }
 }
 
+void timerCallback(){
+  if (frameLastCounter != frameCounter) {
+    Serial.printf("FPS: %d Frames received: %d\n", frameCounter-frameLastCounter, frameCounter);
+    frameLastCounter = frameCounter;
+  } 
+}
+
+// Task sent to the core to decompress + push to Output
+void brTask(void * compressed){  
+  
+  uint8_t *brOutBuffer = new uint8_t[BROTLI_DECOMPRESSION_BUFFER];
+  size_t bufferLength = BROTLI_DECOMPRESSION_BUFFER;
+  BrotliDecoderResult brotli;
+
+  int initMs = micros();
+
+  brotli = BrotliDecoderDecompress(
+    receivedLength,
+    (const uint8_t *)compressed,
+    &bufferLength,
+    brOutBuffer);
+
+  int brotliMs = micros()-initMs;
+  
+    if (brotli) {
+      frameCounter++;      
+      // Send the decompressed buffer to Pixel lib
+      int neoMs = micros();
+      pix.receive(brOutBuffer, bufferLength);
+
+    if (debugMode) {
+        Serial.printf("Neopixels:%d Brotli:%d Total:%d us\n", micros()-neoMs, brotliMs, micros()-initMs);
+        Serial.printf("Decompressed %d bytes for frame %d Heap %d\n", bufferLength, frameCounter, ESP.getFreeHeap());
+      }
+    } else {
+      decompressionFailed++;
+      Serial.printf("Decompression failed %d times after frame: %d\n", decompressionFailed, frameCounter);
+    }
+    
+    delete brOutBuffer;
+    vTaskDelete(NULL);
+}
+
 void connectToWifi() {
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 }
 
-void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
-}
 /**
  * Convert the IP to string 
  */
@@ -84,27 +102,6 @@ String IpAddress2String(const IPAddress& ipAddress)
   String(ipAddress[2]) + String(".") +\
   String(ipAddress[3]);
 }
-/**
- * if (error)
-        {
-          printMessage("After uncompressing json: ",false);printMessage(error.c_str());
-        } else { }
-
- Note this brTask is stripped to the bone to make it as fast as possible
- TODO: Research how to pass an array in notused Parameter (compressed data)
- */
-// Task sent to the core to decompress + push to Output
-void brTask(void * input){
-  AsyncUDPPacket *p = static_cast<AsyncUDPPacket*>(input);  
-  pix.receive(p->data(), p->length());
-
-  if (debugMode) {
-    Serial.println(" Heap: "+String(ESP.getFreeHeap())); 
-  }
-  vTaskDelete(NULL);
-  // https://www.freertos.org/implementing-a-FreeRTOS-task.html
-  // If it is necessary for a task to exit then have the task call vTaskDelete( NULL )
-}
 
 void WiFiEvent(WiFiEvent_t event) {
     Serial.printf("[WiFi-event] event: %d\n", event);
@@ -112,87 +109,43 @@ void WiFiEvent(WiFiEvent_t event) {
     case SYSTEM_EVENT_STA_GOT_IP:
       Serial.println("WiFi connected: ");Serial.println(WiFi.localIP());
 
-      // Read presentation template from FS json only if MQTT_ENABLE is true
-      if (MQTT_ENABLE) {
-      if (SPIFFS.begin())
-      {
-        printMessage("SPIFFS started. Checking for presentation file: ", false);
-        printMessage(FIRMWARE_PRESENTATION);
-        if (SPIFFS.exists(FIRMWARE_PRESENTATION))
-        {
-          printMessage("Presentation file exists. Size in bytes: ", false);
-
-          File configFile = SPIFFS.open(FIRMWARE_PRESENTATION, FILE_READ);
-          presentation = configFile.readString();
-          presentation.replace("<chipid>", bssid);
-          presentation.replace("<udp_ip>", IpAddress2String(WiFi.localIP()));
-          if (configFile)
-          {
-            size_t size = configFile.size();
-            printMessage(String(size));
-            DynamicJsonDocument doc(capacity);
-            // Deserialize the JSON document
-            DeserializationError error = deserializeJson(doc, presentation);
-            if (error)
-            {
-              printMessage("ERR parsing presentation file");
-              printMessage(error.c_str());
-              return;
-            }
-            
-            // TODO: Check if this properties exist in JSON otherwise die here with a proper message
-            strcpy(internalConfig.title, doc["title"]);
-            internalConfig.compression = doc["compression"];
-            printMessage("Controller title: " + String(internalConfig.title));
-            printMessage("=============== Presentation ready ===============");
-            printMessage(presentation);
-          }
-          else
-          {
-            printMessage("ERR load config");
-          }
-          configFile.close();
-        }
-        else
-        {
-          printMessage("Config file not found");
-        }
-      }
-      else
-      {
-        printMessage("SPIFFs cannot start. FFS Formatted?");
-      }
-    
-        connectToMqtt(); 
-      } else {
-        Serial.println("MQTT_ENABLE is disabled per Config. Spiffs presentation read skipped");
-      }
-
       if(udp.listen(UDP_PORT)) {
         Serial.println("UDP Listening on IP: ");
         Serial.print(IpAddress2String(WiFi.localIP())+":");
         Serial.println(UDP_PORT);
 
+      // Interval to measure FPS  (millis, function called, times invoked for 1000ms around 1 hr and half)
+      timer.setTimer(1000, timerCallback, 6000);
+
     // Executes on UDP receive
     udp.onPacket([](AsyncUDPPacket packet) {
-        //printMessage("UDP packet from: ",false);printMessage(String(packet.remoteIP()));
-        printMessage("Len: ", false);
-        printMessage(String(packet.length()), true);
-        unsigned long t = micros();
-        pix.receive(packet.data(), packet.length());
-        Serial.print(" Took ");
-        Serial.print(micros()-t);
-        Serial.println(" micro seconds to consume");
-        delay(0);
+      
+        if ((int) packet.data()[0] == 80) {
+        // Non compressed
+          pix.receive(packet.data(), packet.length());
+        } else {
+          receivedLength = packet.length();
+
+          xTaskCreatePinnedToCore(
+                    brTask,        
+                    "uncompress", 
+                    10000,         
+                    packet.data(),   
+                    9,            
+                    &brotliTask,  
+                    0);           
+          delay(1);  
+        }
+
         }); 
+ 
     } else {
       Serial.println("UDP Lister could not be started");
     }
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
         Serial.println("WiFi lost connection");
-        // Ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
-        xTimerStop(mqttReconnectTimer, 0);
+        
 	    	xTimerStart(wifiReconnectTimer, 0);
         break;
         // Non used, just there to avoid warnings
@@ -224,95 +177,21 @@ void WiFiEvent(WiFiEvent_t event) {
     }
 }
 
-void onMqttConnect(bool sessionPresent) {
-  printMessage("Connected to MQTT. Sending presentation to topic: ", false);
-  printMessage(PRESENTATION_TOPIC);
-  printMessage("Last will: ", false);
-  printMessage(lastWill);
-  mqttClient.publish(lastWill.c_str(), 1, true, "true");
-  // TODO: Read presentation from JSON and publish to PRESENTATION_TOPIC
-  mqttClient.publish(PRESENTATION_TOPIC, 1, true, presentation.c_str());
-  
-  // Subscribe to receive topic (TODO: Later this should be dynamic)
-  printMessage("Subscribing to IN_TOPIC, packetId: ", false);
-  uint16_t packetIdSub = mqttClient.subscribe(IN_TOPIC, 0);
-  printMessage(String(packetIdSub));
-}
-
-void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected()) {
-    xTimerStart(mqttReconnectTimer, 0);
-  }
-}
-
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  printMessage("  packetId: ");
-  printMessage(String(packetId));
-  printMessage("  qos: ");
-  printMessage(String(qos));
-}
-
-void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, unsigned int len, size_t index, size_t total) {
-// TODO: This topic should be dynamic 
-   if (0 == strcmp(topic, "pixelcrasher/pixel-in/4ae5b9/hsl"))
-  {
-    // Enable buffering of partial messages. TODO: Do this with char not with String!
-    if (index==0) {               // at start
-    payloadBuffer = "";
-    }
-    payloadBuffer += payload;
-    if (! (index + len == total)) { // at end
-      return;
-    }  
-  }
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
 void setup()
 {
   Serial.begin(115200);
-  // Set chip id in hex format
-  itoa(ESP.getEfuseMac(), bssid, 16);
-  strcpy(internalConfig.title, bssid);
+  pix.init();
   
+  Serial.printf("UDPX %s\n", UDPX_VERSION); 
+
   // Set up automatic reconnect timers
-  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
   connectToWifi();
-  pix.init();
-  WiFi.onEvent(WiFiEvent);
-  // TODO: Update this with new define STATUS_TOPIC
-  lastWill = "pixelcrasher/online-status/"+String(bssid);
 
-  // keepalive period, broker sets device offline after this period
-  mqttClient.setKeepAlive(15);
-  mqttClient.setMaxTopicLength(2000);
-  mqttClient.setWill(lastWill.c_str(), 1, true, "false");
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  // Pixels output
-  
+  WiFi.onEvent(WiFiEvent);
+
 }
 
 void loop() {
-  delay(0);
+  timer.run();
 }
