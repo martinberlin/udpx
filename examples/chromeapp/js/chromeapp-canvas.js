43165b2a0aba2e87d17683605d771ee133a29ff6
martinberlin <martin@luckycloud.de>
2020-01-19 10:45:35 +0100

    Chromeapp fixes: Note that chromeapp will be not be supported / updated anymore after February 1, 2020

--- examples/chromeapp/js/chromeapp-canvas.js
+++ examples/chromeapp/js/chromeapp-canvas.js
@@ -1,19 +1,23 @@
-let v = document.getElementById('video');
-let canvas = document.getElementById('c');
+let d = document;
+let v = d.getElementById('video');
+let canvas = d.getElementById('c');
 let context = canvas.getContext('2d');
-let ip = document.getElementById('esp32_ip'),
-    port = document.getElementById('udp_port'),
-    contrast = document.getElementById('v_contrast'),
-    v_width  = document.getElementById('v_width'),
-    v_height = document.getElementById('v_height'),
-    v_units  = document.getElementById('v_units'),
-    video = document.getElementById('video'),
-    video_select = document.getElementById('video_select'),
-    millis_frame = document.getElementById('millis_frame'),
-    protocol = document.getElementById('protocol'),
-    transmission = document.getElementById('transmission'),
-    quality = document.getElementById('bro_quality'),
-    v_brightness = document.getElementById('v_brightness');
+let ip = d.getElementById('esp32_ip'),
+    port = d.getElementById('udp_port'),
+    contrast = d.getElementById('v_contrast'),
+    v_width  = d.getElementById('v_width'),
+    v_height = d.getElementById('v_height'),
+    v_units  = d.getElementById('v_units'),
+    video = d.getElementById('video'),
+    video_select = d.getElementById('video_select'),
+    millis_frame = d.getElementById('millis_frame'),
+    protocol = d.getElementById('protocol'),
+    transmission = d.getElementById('transmission'),
+    quality = d.getElementById('bro_quality'),
+    v_brightness = d.getElementById('v_brightness');
+let o_chunk = d.getElementById('o_chunk'), o_chunk_label = d.getElementById('o_chunk_label'),o_chunk_pre = "Set header chunk size to ";
+let m_rotate_lines = d.getElementById('m_rotate_lines'),
+m_invert_unit = d.getElementById('m_invert_unit');
 let t_empty = '&#9633;';
 let socketId, oldPort = port.value; 
 let cw = parseInt(v_width.value),
@@ -21,8 +25,8 @@ ch = parseInt(v_height.value)*parseInt(v_units.value),
 unitH = parseInt(v_height.value);
 let ua = navigator.userAgent.toLowerCase();
 let isAndroid = ua.indexOf("android") > -1;
-
-document.addEventListener('DOMContentLoaded', function(){
+let adv_invert = 0;
+d.addEventListener('DOMContentLoaded', function(){
     openSocket();
     cleanTransmission();
     /**
@@ -49,10 +53,22 @@ document.addEventListener('DOMContentLoaded', function(){
     port.onchange = function() {
         cleanTransmission();
     };
+    m_invert_unit.onchange = function() {
+        saveFormState();
+       if (m_invert_unit.checked) {
+         adv_invert = -1;
+        } else {
+         adv_invert = 0;
+       }
+     }
+    v_width.onchange = function() {
+        o_chunk_label.innerText = o_chunk_pre + (v_width.value * v_height.value);
+    }
     v_height.onchange = function() {
         cleanTransmission();
         ch = parseInt(v_height.value)*parseInt(v_units.value);
         canvas.height = ch;
+        o_chunk_label.innerText = o_chunk_pre + (v_width.value * v_height.value);
     }
     v_units.onchange = function() {
         ch = parseInt(v_height.value)*parseInt(v_units.value);
@@ -136,72 +152,109 @@ function sendUdp(bytesToPost) {
 
 }
 
+
 function convertChannel(pixels) {
     let pixLength = pixels.length;
     // Line data flow direction
     // ----> Line 1
     // <---- Line 2
     // ----> Line 3  ...
-    // ----> Line 12 (module 2)
+    // ----> Line 12 (in 2nd Unit but only if the units have impair rows like 11)
     let lineCount = 1;
     let cw = parseInt(v_width.value);
-    for (var x = 0; x <= pixLength-cw; x=x+cw) {
-        // Pair modules are mirrored
-        let isModuleImpair = (lineCount <= unitH) ? 0 : 1;
-        // Invert pixels in pair lines for this Led matrix 
-        if (lineCount % 2 === isModuleImpair) {
-            let pixelsInvertedCopy = pixels.slice(x,x+cw);
-            pixelsInvertedCopy.reverse();
 
-            let invIndex = 0;
-            for (var inv = x; inv <= x+cw-1; inv++) {  
-                pixels[inv] = pixelsInvertedCopy[invIndex];
-                invIndex++
+    if (m_rotate_lines.checked) {
+        let invert = false;
+
+        for (var x = 0; x <= pixLength-cw; x=x+cw) {
+
+            // Invert pixels in pair lines for this Led matrix
+            if (invert) {
+                let pixelsInvertedCopy = pixels.slice(x,x+cw);
+                pixelsInvertedCopy.reverse();
+
+                let invIndex = 0;
+                for (var inv = x; inv <= x+cw-1; inv++) {
+                    pixels[inv] = pixelsInvertedCopy[invIndex];
+                    invIndex++
+                }
             }
-            //console.log("x:"+x+" Inverted line:"+lineCount+ " inv:"+inv, pixels);
+            if ((lineCount % unitH === adv_invert) === false) {
+              invert = !invert;
+            }
+            lineCount++;
         }
-        lineCount++;
+
     }
     
     let MSB = parseInt(pixLength/256);
     let LSB = pixLength - (MSB*256);
-    headerBytes = 6;
+
+    let cMSB = 0;
+    let cLSB = 0;
+    if (o_chunk.checked) {
+        let chunk_size = v_width.value*v_height.value;
+        cMSB = parseInt(chunk_size/256);
+        cLSB = chunk_size - (cMSB*256);
+    }
+
+    let headerBytes = 6;
+    let bytesPerPixel = 3;
     // Header bytes 
     switch (protocol.value) {
         case 'rgb888':
             hByte = [1,0,0,0,LSB,MSB];
         break;
+        case 'rgb565':
+            hByte = [3,0,0,0,LSB,MSB];
+        break;
         case 'bro888':
             hByte = [14,0,0,0,LSB,MSB];
         break;
+        case 'pix565':
+            hByte = [82,0,cLSB,cMSB,LSB,MSB];
+            bytesPerPixel = 2;
+        break;
         default:
-            // 1: p  2: Non used  3 Channel   4 Length LSB   5 Length MSB
-            hByte = [80,0,0,LSB,MSB];
-            headerBytes = 5;
+            // 1: p  2: Chunk LSB  3: Chunk MSB  4: Length LSB  5: Length MSB  6: protocol (0 pixels)
+            hByte = [80,0,cLSB,cMSB,LSB,MSB];
         break;
       }
-    //console.log(hByte); // Debug headers
-    let bufferLen = (pixLength*3)+headerBytes;
+
+    let bufferLen = (pixLength*bytesPerPixel)+headerBytes;
     // create an ArrayBuffer with a size in bytes
     var buffer = new ArrayBuffer(bufferLen);
     var bytesToPost = new Uint8Array(buffer); 
     bi = 0;
     bytesToPost[bi] = hByte[0];bi++;  // p
-    bytesToPost[bi] = hByte[1];bi++;  // Future features (not used)
-    bytesToPost[bi] = hByte[2];bi++;  // unsigned 8-bit LED channel number
-    bytesToPost[bi] = hByte[3];bi++;  // count(pixels) 16 bit, next too
-    bytesToPost[bi] = hByte[4];bi++;  // Second part of count(pixels) not used here for now
-  if (protocol.value === 'rgb888') {
-    bytesToPost[bi] = hByte[5];bi++;
-  }
+    bytesToPost[bi] = hByte[1];bi++;  // chunk (16 bit)
+    bytesToPost[bi] = hByte[2];bi++;  //
+    bytesToPost[bi] = hByte[3];bi++;  // count(pixels) 16 bit
+    bytesToPost[bi] = hByte[4];bi++;  //
+    bytesToPost[bi] = hByte[5];bi++;  // protocol
+    let r,g,b;
     for (var k = 0; k < pixLength; k++) {
-        bytesToPost[bi] = Math.round(pixels[k][0]*v_brightness.value);bi++;
-        bytesToPost[bi] = Math.round(pixels[k][1]*v_brightness.value);bi++;
-        bytesToPost[bi] = Math.round(pixels[k][2]*v_brightness.value);bi++;
+        r = Math.round(pixels[k][0]*v_brightness.value);
+        g = Math.round(pixels[k][1]*v_brightness.value);
+        b = Math.round(pixels[k][2]*v_brightness.value);
+
+        if (protocol.value === 'pix565' || protocol.value === 'rgb565') {
+        // 565
+        let rgbsum = Math.round(b/8)+Math.round(g/4)*32+Math.round(r/8)*2048;
+        let first565 = Math.round(rgbsum/256);
+        bytesToPost[bi] = first565;bi++;
+        bytesToPost[bi] = Math.round(rgbsum-first565*256);bi++;
+
+        } else {
+        // pixels
+        bytesToPost[bi] = r;bi++;
+        bytesToPost[bi] = g;bi++;
+        bytesToPost[bi] = b;bi++;
+        }
     }
+
     sendUdp(bytesToPost);
   }
-
   
 function draw(v,c,w,h) {
     if(v.paused || v.ended) return false;
